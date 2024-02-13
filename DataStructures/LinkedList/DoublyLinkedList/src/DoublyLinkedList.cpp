620c007f12773bac0514740c3f6aebecdd775fa6
vezzolter <vezzolter@gmail.com>
2024-02-13 14:43:00 +0200

    Add code in DLL: default cstr, copy cstr, operator=, pushBack.

---
+++ DataStructures/LinkedList/DoublyLinkedList/src/DoublyLinkedList.cpp
@@ -0,0 +1,285 @@
+// Source file for simplified ADT: Singly Linked List
+// by vezzolter
+// February 3, 2024
+
+#ifndef DLL_CPP
+#define DLL_CPP
+
+#include "DoublyLinkedList.h"
+
+// ------------------------
+// Special Member Functions
+// ------------------------
+
+// Default Constructor
+template<typename T>
+DLL<T>::DLL() : _size(0), _head(nullptr), _tail(nullptr) {}
+
+// Deep Copy Constructor
+template<class T>
+DLL<T>::DLL(const DLL& rhs) {
+	// Set corresponding size
+	_size = rhs._size;
+
+	// Case: empty list, avoid dangling pointers
+	if (rhs._head == nullptr) {
+		_head = _tail = nullptr;
+		return;
+	}
+
+	// Create corresponding node
+	_head = new Node<T>(rhs._head->_data);
+
+	// Initialize traversal pointers
+	Node<T>* currentRhs = rhs._head->_next;
+	Node<T>* current = _head;
+
+	// Copy elements
+	while (currentRhs) {
+		current->_next = new Node<T>(currentRhs->_data);
+		current->_next->_prev = current;
+		current = current->_next;
+		currentRhs = currentRhs->_next;
+	}
+
+	// Maintain proper reference to the end
+	_tail = current;
+}
+
+// Deep copy assignment operator
+template<class T>
+DLL<T>& DLL<T>::operator=(const DLL& rhs) {
+	// Self-assignment guard
+	if (this == &rhs) 
+		return *this;
+
+	// Ensure that the destination list doesn't retain any of its existing elements
+	clear();
+
+	// Set corresponding size
+	_size = rhs._size;
+
+	// Case: empty list, avoid dangling pointers
+	if (rhs._head == nullptr) {
+		_head = _tail = nullptr;
+		return *this;
+	}
+
+	// Create corresponding node
+	_head = new Node<T>(rhs._head->_data);
+
+	// Initialize traversal pointers
+	Node<T>* currentRhs = rhs._head->_next;
+	Node<T>* current = _head;
+
+	// Copy elements
+	while (currentRhs) {
+		current->_next = new Node<T>(currentRhs->_data);
+		current->_next->_prev = current;
+		current = current->_next;
+		currentRhs = currentRhs->_next;
+	}
+
+	// Maintain proper reference to the end
+	_tail = current;
+	return *this;
+}
+
+// Desctuctor
+template<typename T>
+DLL<T>::~DLL() { clear(); }
+
+
+
+// --------------
+// Element Access
+// --------------
+
+// Access the element at the specified index with modification
+template<class T>
+T& DLL<T>::operator[](const int index) {
+	// TODO: potential range check
+
+	// For traversing purposes
+	int counter = 0;
+	Node<T>* current = _head;
+
+	// Loop until end (nullptr)
+	while (current != nullptr) {
+		// Return value of matched node
+		if (counter == index) {
+			return current->_data;
+		}
+		// Otherwise keep traversing
+		current = current->_next;
+		counter++;
+	}
+}
+
+// Access the element at the specified index without modification
+template<class T>
+T& DLL<T>::operator[](const int index) const {
+	// TODO: potential range check
+
+	// For traversing purposes
+	int counter = 0;
+	Node<T>* current = _head;
+
+	// Loop until end (nullptr)
+	while (current != nullptr) {
+		// Return value of matched node
+		if (counter == index) {
+			return current->_data;
+		}
+		// Otherwise keep traversing
+		current = current->_next;
+		counter++;
+	}
+}
+
+// Returns a reference to the first element in the container.
+template<class T>
+T& DLL<T>::front() {
+	// TODO: handle empty case
+
+	return _head->_data;
+}
+
+
+
+// --------
+// Capacity
+// --------
+
+// Checks if the container has no elements
+template<class T>
+bool DLL<T>::empty() const { return _size == 0; }
+
+// Returns the number of elements in the container
+template<typename T>
+int DLL<T>::size() const { return _size; }
+
+
+
+// ---------
+// Modifiers
+// ---------
+
+// Erases all elements from the container.
+template<typename T>
+void DLL<T>::clear() {
+	// Loop until end (nullptr)
+	while (_head) {
+		// Create temp node for current, so it can be deleted later
+		Node<T>* current = _head;
+		// Traverse via head
+		_head = _head->_next;
+		// Delete previous
+		delete current;
+	}
+	// Update the size
+	_size = 0;
+}
+
+// Inserts elements after the specified position in the container
+template<class T>
+void DLL<T>::insertAfter(const int index, const T& newData) {
+	// TODO: range check
+
+	// Create a new node with the given data
+	Node<T>* newNode = new Node<T>(newData);
+
+	// Find the node at the specified index
+	Node<T>* current = _head;
+	for (int i = 0; i < index; i++) {
+		current = current->_next;
+	}
+
+	// Insert the new node after the current one
+	newNode->_next = current->_next;
+	current->_next = newNode;
+
+	// Update the size
+	_size++;
+}
+
+// Removes an element at the specified position
+template<class T>
+void DLL<T>::eraseAfter(const int index) {
+	// TODO: range check
+
+	// Traverse to the node before the node to be erased
+	Node<T>* current = _head;
+	for (int i = 0; i < index; ++i) {
+		current = current->_next;
+	}
+
+	// Store a pointer to the node to be removed
+	Node<T>* temp = current->_next;
+	// Update the next pointer of the current node to skip over the node to be removed
+	current->_next = temp->_next;
+	// Free up memory
+	delete temp;
+	// Update the size
+	_size--;
+}
+
+// Prepends the given element value to the beginning of the container.
+template<typename T>
+void DLL<T>::pushFront(const T& newData) {
+	// Create a new node with the given data
+	Node<T>* newNode = new Node<T>(newData);
+
+	// If list has elements
+	if (_head != nullptr) {
+		// Point newNode to current head
+		newNode->_next = _head;
+	}
+
+	// Update the head
+	_head = newNode;
+	// Update the size
+	_size++;
+}
+
+// Removes the first element of the container.
+template<class T>
+void DLL<T>::popFront() {
+	// TODO: range check
+
+	// Move the head pointer to the next node
+	Node<T>* temp = _head;
+	_head = _head->_next;
+
+	// Delete the original head node
+	delete temp;
+	// Update the size
+	_size--;
+}
+
+// Appends the given element to the end of the container.
+template<class T>
+void DLL<T>::pushBack(const T& newData) {
+	// Create a new node with the given data
+	Node<T>* newNode = new Node<T>(newData);
+
+	// Append node to the head if list is empty, otherwise to the end
+	if (_head == nullptr) {
+		_head = newNode;
+	}
+	else {
+		Node<T>* tail = _head;
+		while (tail->_next != nullptr) {
+			tail = tail->_next;
+		}
+
+		// Link the new node to the last one
+		tail->_next = newNode;
+		newNode->_prev = tail;
+	}
+
+	// Update the size
+	_size++;
+}
+			
+#endif
---
+++ DataStructures/LinkedList/DoublyLinkedList/src/Main.cpp
@@ -0,0 +1,69 @@
+// Simplified ADT: Doubly Linked List
+// by vezzolter
+// February 13, 2024
+//
+// This C++ project demonstrates the implementation of the
+// simplified version of abstract data type called doubly linked list.
+
+#include <iostream>
+#include "DoublyLinkedList.h"
+
+void printList(const DLL<int>& list) {
+	std::cout << "Elements:\t";
+	for (int i = 0; i < list.size(); i++) {
+		std::cout << list[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	// Greetings
+	std::cout << "Welcome to the 'Singly Linked List' console application!\n\n";
+
+	// Create initial list
+	std::cout << "Creating & filling initial list #1...\n";
+	DLL<int> list1;
+	list1.pushFront(9);
+	list1.pushFront(1);
+	list1.pushFront(1);
+	list1.pushFront(7);
+
+	// Show it
+	std::cout << "Is it empty:\t" << list1.empty() << std::endl;
+	printList(list1);
+
+	// Modify it
+	std::cout << "\nChange first element ('7') to '5'...\n";
+	list1.front() = 5;
+	printList(list1);
+
+	// Insert to it
+	std::cout << "\nInsert element '3' before '9'...\n";
+	list1.insertAfter(list1.size() - 2, 3);
+	printList(list1);
+
+	// Deep copy mechanics
+	std::cout << "\nCreate a list copies and compare...\n";
+	DLL<int> list2(list1);
+	DLL<int> list3 = list1;
+	printList(list1);
+	printList(list2);
+	printList(list3);
+
+	// Remove from first
+	std::cout << "\nFrom list #1 remove first ('5') and last ('9') elements...\n";
+	list1.popFront();
+	list1.eraseAfter(list1.size() - 2);
+	printList(list1);
+	printList(list2);
+	printList(list3);
+
+	// Exiting
+	std::cout << "\nThanks for using this program! Have a great day!\n";
+	std::cout << "Press <Enter> to exit...";
+	std::cin.clear(); // ensure that stream is in a good state
+	std::cin.ignore(32767, '\n'); // clear from any remaining chars
+	std::cin.get();
+	return 0;
+}
