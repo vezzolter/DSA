6c2896f9cdeefa105a05a8e3e8b3e10ed3bb9466
vezzolter <vezzolter@gmail.com>
2024-07-25 12:42:58 +0300

    Establish: 'Counting Sort'.

---
+++ Algorithms/Sorting/CountingSort/Include/CountingSort.h
@@ -0,0 +1,13 @@
+// Header file for Counting Sort
+// by vezzolter
+// July 25, 2024
+
+
+#ifndef COUNTINGSORT_H
+#define COUNTINGSORT_H
+
+
+void countingSort();
+
+
+#endif // COUNTINGSORT_H
---
+++ Algorithms/Sorting/CountingSort/Source/CountingSort.cpp
@@ -0,0 +1,11 @@
+// Source file for Counting Sort
+// by vezzolter
+// July 25, 2024
+
+
+#include "CountingSort.h"
+
+
+void countingSort() {
+
+}
