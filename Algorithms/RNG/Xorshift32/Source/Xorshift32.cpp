3b4ea681a2a9f0e0658a1f424beb9a32c2215adf
vezzolter <vezzolter@gmail.com>
2025-02-06 16:17:09 +0200

    Fix 'Xorshift32': second shift should be to the right.

--- Algorithms/RNG/Xorshift32/Source/Xorshift32.cpp
+++ Algorithms/RNG/Xorshift32/Source/Xorshift32.cpp
@@ -16,7 +16,7 @@ Xorshift32::Xorshift32(uint32_t seed) {
 // Generates the next pseudorandom number using the Xorshift32 transformations
 uint32_t Xorshift32::generate() {
 	state ^= state << 13;
-	state ^= state << 17;
+	state ^= state >> 17;
 	state ^= state << 5;
 
 	return state;
