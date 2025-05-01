1b090aec13410c9bc6c80fcb0e1c738ef98ed50a
vezzolter <vezzolter@gmail.com>
2025-05-01 16:32:32 +0300

    Add 'Splay Tree' code: establish structure.

--- DataStructures/Tree/SplayTree/Include/SplayTree.h
+++ DataStructures/Tree/SplayTree/Include/SplayTree.h
@@ -8,7 +8,222 @@
 #define SPLAYTREE_H
 
 
-// Declaration
+class SplayTree {
+private:
+    int _size;
+    struct Node;
+    Node* _root;
+
+    // -----------------
+    //  Utility Methods
+    // -----------------
+    // Declaration
+
+public:
+    // --------------------
+    //  Compiler Generated
+    // --------------------
+    SplayTree();
+    SplayTree(const SplayTree& other);
+    SplayTree(SplayTree&& other)          = delete;
+    SplayTree& operator=(const SplayTree& rhs);
+    SplayTree& operator=(SplayTree&& rhs) = delete;
+    ~SplayTree();
+
+    // -----------
+    //  Iterators
+    // -----------
+    class Iterator;
+    using iterator = Iterator;
+    iterator begin();
+    iterator end();
+    class ConstIterator;
+    using const_iterator = ConstIterator;
+    const_iterator begin() const;
+    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+    // ----------------
+    //  Element Access
+    // ----------------
+    iterator find(const int& val);
+    const_iterator find(const int& val) const;
+    iterator predecessor(const int& val);
+    const_iterator predecessor(const int& val) const;
+    iterator successor(const int& val);
+    const_iterator successor(const int& val) const;
+    iterator predecessor(const iterator& it);
+    const_iterator predecessor(iterator& it) const;
+    iterator successor(const iterator& it);
+    const_iterator successor(iterator& it) const;
+    int minimum() const;
+    int maximum() const;
+
+    // ----------
+    //  Capacity
+    // ----------
+    bool empty() const;
+    int size() const;
+    int height() const;
+    int height(const int& val) const;
+    int height(const iterator& it) const;
+    int depth() const;
+    int depth(const int& val) const;
+    int depth(const iterator& it) const;
+
+    // -----------
+    //  Modifiers
+    // -----------
+    void insert(const int& val);
+    void remove(const int& val);
+    void clear();
+    void swap(SplayTree& other);
+};
+
+
+struct SplayTree::Node {
+public:
+    int data;
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    // --------------------
+    //  Compiler Generated
+    // --------------------
+    Node();
+    Node(const int& val, Node* parent = nullptr);
+    Node(const Node& other)          = delete; // no copying or moving to ensure 
+    Node(Node&& other)               = delete; // uniqueness of the node within    
+    Node& operator=(const Node& rhs) = delete; // the tree and prevent accidental 
+    Node& operator=(Node&& rhs)      = delete; // duplicates or dangling nodes
+    ~Node()                          = default;
+};
+
+
+class SplayTree::Iterator {
+private:
+    friend class SplayTree; // to handle iterator-based methods in SplayTree
+    Node* _curr;
+
+    // -----------------
+    //  Utility Methods
+    // -----------------
+
+    // Find the leftmost node starting from the given node (the smallest)
+    static Node* findLeftmost(Node* node) {
+        // Implementation
+    }
+
+    // Find the next node of the given node (in-order successor)
+    static Node* findNext(Node* node) {
+        // Implementation
+    }
+
+public:
+    // --------------------
+    //  Compiler Generated
+    // --------------------
+    Iterator();
+    explicit Iterator(Node* node);
+    Iterator(const Iterator& other)          = default;
+    Iterator(Iterator&& other)               = default;
+    Iterator& operator=(const Iterator& rhs) = default;
+    Iterator& operator=(Iterator&& rhs)      = default;
+    ~Iterator()                              = default;
+
+    // ----------------------
+    //  Overloaded Operators
+    // ----------------------
+
+    // Returns a reference to the data of the current node
+    int& operator*() {
+        // Implementation
+    }
+
+    // Advances the iterator to the next element in in-order traversal (pre-increment)
+    Iterator& operator++() {
+        // Implementation
+    }
+
+    // Advances the iterator to the next element, returning the previous state (post-increment)
+    Iterator operator++(int) {
+        // Implementation
+    }
+
+    // Returns true if two iterators point to the same node
+    friend bool operator==(const SplayTree::Iterator& lhs, const SplayTree::Iterator& rhs) {
+        // Implementation
+    }
+
+    // Returns true if two iterators point to different nodes
+    friend bool operator!=(const SplayTree::Iterator& lhs, const SplayTree::Iterator& rhs) {
+        // Implementation
+    }
+};
+
+
+class SplayTree::ConstIterator {
+private:
+    friend class SplayTree; // to handle iterator-based methods in SplayTree
+    const Node* _curr;
+
+    // -----------------
+    //  Utility Methods
+    // -----------------
+
+    // Find the leftmost node starting from the given node (the smallest)
+    static const Node* findLeftmost(Node* node) {
+        // Implementation
+    }
+
+    // Find the next node of the given node (in-order successor)
+    static const Node* findNext(const Node* node) {
+        // Implementation
+    }
+
+public:
+    // --------------------
+    //  Compiler Generated
+    // --------------------
+    ConstIterator();
+    ConstIterator(const Node* node);
+    ConstIterator(const ConstIterator& other)          = default;
+    ConstIterator(ConstIterator&& other)               = default;
+    ConstIterator& operator=(const ConstIterator& rhs) = default;
+    ConstIterator& operator=(ConstIterator&& rhs)      = default;
+    ~ConstIterator()                                   = default;
+
+    // ----------------------
+    //  Overloaded Operators
+    // ----------------------
+
+    // Returns a const reference to the data of the current node
+    const int& operator*() const {
+        // Implementation
+    }
+
+    // Advances the iterator to the next element in in-order traversal (pre-increment)
+    ConstIterator& operator++() {
+        // Implementation
+    }
+
+    // Advances the iterator to the next element, returning the previous state (post-increment)
+    ConstIterator operator++(int) {
+        // Implementation
+    }
+
+    // Returns true if two iterators point to the same node
+    friend bool operator==(const SplayTree::ConstIterator& lhs, const SplayTree::ConstIterator& rhs) {
+        // Implementation
+    }
+
+    // Returns true if two iterators point to different nodes
+    friend bool operator!=(const SplayTree::ConstIterator& lhs, const SplayTree::ConstIterator& rhs) {
+        // Implementation
+    }
+};
 
 
 #endif // SPLAYTREE_H
--- DataStructures/Tree/SplayTree/Source/SplayTree.cpp
+++ DataStructures/Tree/SplayTree/Source/SplayTree.cpp
@@ -7,4 +7,191 @@
 #include "SplayTree.h"
 
 
-// Definition
+// -----------------
+//  Utility Methods
+// -----------------
+
+// Implementation
+
+
+// --------------------
+//  Compiler Generated
+// --------------------
+
+// Constructs an empty SplayTree
+SplayTree::SplayTree() {
+	// Implementation
+}
+
+// Constructs a SplayTree with the deep copy contents of 'other'
+SplayTree::SplayTree(const SplayTree& other) {
+	// Implementation
+}
+
+// Replaces the contents of this SplayTree with a deep copy of 'rhs'
+SplayTree& SplayTree::operator=(const SplayTree& rhs) {
+	// Implementation
+}
+
+// Performs final cleanup and terminates the object
+SplayTree::~SplayTree() {
+	// Implementation
+}
+
+
+// -----------
+//  Iterators
+// -----------
+
+// Returns an iterator to the first element of the SplayTree
+SplayTree::iterator SplayTree::begin() { }
+
+// Returns an iterator to one past the last element of the SplayTree
+SplayTree::iterator SplayTree::end() {  }
+
+// Returns a const iterator to the first element of the SplayTree
+SplayTree::const_iterator SplayTree::begin() const { }
+
+// Returns a const iterator to one past the last element of the SplayTree
+SplayTree::const_iterator SplayTree::end() const { }
+
+// Explicitly returns a const iterator to the first element of the SplayTree
+SplayTree::const_iterator SplayTree::cbegin() const { }
+
+// Explicitly returns a const iterator to one past the last element of the SplayTree
+SplayTree::const_iterator SplayTree::cend() const {  }
+
+
+// ----------------
+//  Element Access 
+// ----------------
+
+// Returns an iterator to the element with given value
+SplayTree::iterator SplayTree::find(const int& val) {
+	// Implementation
+}
+
+// Returns a const iterator to the element with given value
+SplayTree::const_iterator SplayTree::find(const int& val) const {
+	// Implementation
+}
+
+// Returns an iterator to the predecessor of the given value
+SplayTree::iterator SplayTree::predecessor(const int& val) {
+	// Implementation
+}
+
+// Returns a const_iterator to the predecessor of the given value
+SplayTree::const_iterator SplayTree::predecessor(const int& val) const {
+	// Implementation
+}
+
+// Returns an iterator to the successor of the given value
+SplayTree::iterator SplayTree::successor(const int& val) {
+	// Implementation
+}
+
+// Returns a const_iterator to the successor of the given value
+SplayTree::const_iterator SplayTree::successor(const int& val) const {
+	// Implementation
+}
+
+// Returns an iterator to the predecessor of the node pointed to by the given iterator
+SplayTree::iterator SplayTree::predecessor(const iterator& it) {
+	// Implementation
+}
+
+// Returns a const_iterator to the predecessor of the node pointed to by the given iterator
+SplayTree::const_iterator SplayTree::predecessor(iterator& it) const {
+	// Implementation
+}
+
+// Returns an iterator to the successor of the node pointed to by the given iterator
+SplayTree::iterator SplayTree::successor(const iterator& it) {
+	// Implementation
+}
+
+// Returns a const_iterator to the successor of the node pointed to by the given iterator
+SplayTree::const_iterator SplayTree::successor(iterator& it) const {
+	// Implementation
+}
+
+// Returns the minimum value in the tree
+int SplayTree::minimum() const {
+	// Implementation
+}
+
+// Returns the maximum value in the tree
+int SplayTree::maximum() const {
+	// Implementation
+}
+
+
+// ----------
+//  Capacity
+// ----------
+
+// Returns true if the tree has no elements
+bool SplayTree::empty() const {
+	// Implementation
+}
+
+// Returns the total number of elements in the tree
+int SplayTree::size() const {
+	// Implementation
+}
+
+// Returns the height of the entire tree
+int SplayTree::height() const {
+	// Implementation
+}
+
+// Returns the height of the subtree rooted at the given value
+int SplayTree::height(const int& val) const {
+	// Implementation
+}
+
+// Returns the height of the subtree rooted at the given iterator
+int SplayTree::height(const iterator& it) const {
+	// Implementation
+}
+
+// Returns the depth of the entire tree
+int SplayTree::depth() const {
+	// Implementation
+}
+
+// Returns the depth of the node with the given value
+int SplayTree::depth(const int& val) const {
+	// Implementation
+}
+
+// Returns the depth of the node given an iterator
+int SplayTree::depth(const iterator& it) const {
+	// Implementation
+}
+
+
+// -----------
+//  Modifiers
+// -----------
+
+// Inserts a new element into the SplayTree, maintaining ordering and balance 
+void SplayTree::insert(const int& val) {
+	// Implementation
+}
+
+// Removes a node with the given value from the SplayTree, maintaining order and balance
+void SplayTree::remove(const int& val) {
+	// Implementation
+}
+
+// Removes all nodes from the tree and resets it to an empty state
+void SplayTree::clear() {
+	// Implementation
+}
+
+// Exchanges the contents of this SplayTree with another SplayTree
+void SplayTree::swap(SplayTree& other) {
+	// Implementation
+}
